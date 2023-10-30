b6b7f0ad3661af37c7707d4b8e71501c9e53c173
MNischida <mgonischida@gmail.com>
2023-10-30 20:24:25 -0300

    step

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -13,8 +13,6 @@ define([
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
 
     connection.on('clickedNext', onClickedNext);
     connection.on('clickedBack', onClickedBack);
@@ -23,8 +21,7 @@ define([
     function onRender() {
         connection.trigger('ready');
 
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
+        gotoStep(step);
 
         $('#field1').change(function () {
             connection.trigger('updateButton', {
@@ -104,16 +101,6 @@ define([
                 save();
                 break;
         }
-    }
-
-    function onGetTokens(tokens) {
-    // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-    // console.log(tokens);
-    }
-
-    function onGetEndpoints(endpoints) {
-    // Response: endpoints = { restHost: <url> } i.e. 'rest.s1.qa1.exacttarget.com'
-    // console.log(endpoints);
     }
 
     function save() {
