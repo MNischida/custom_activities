356b91542f93d465e4b1ea0c685636bf3d5c072e
MNischida <mgonischida@gmail.com>
2023-11-01 12:06:45 -0300

    teste

--- public/js/jwtDecoder.js
+++ public/js/jwtDecoder.js
@@ -5,7 +5,19 @@ module.exports = (body) => {
         return new Error('Invalid jwtdata');
     }
 
-    return JWT.verify(body.toString('utf8'), process.env.JWT, {
-        algorithm: 'HS256'
-    });
+    console.log(body);
+
+    console.log('Teste: ' + JSON.stringify(body))
+
+
+    try {
+        const decoded = JWT.verify(body.toString('utf8'), process.env.JWT);
+        console.log('Token verificado com sucesso:', decoded);
+      } catch (error) {
+        console.error('Erro na verificação do token:', error.message);
+      }
+
+    // return JWT.verify(body.toString('utf8'), process.env.JWT, {
+    //     algorithm: 'HS256'
+    // });
 };
