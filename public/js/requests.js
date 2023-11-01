a80d8c70c37e8d40495c5590a41640f45b1c84c3
MNischida <mgonischida@gmail.com>
2023-11-01 18:07:58 -0300

    stringify payload sms

--- public/js/requests.js
+++ public/js/requests.js
@@ -37,7 +37,7 @@ module.exports = (type, telefone, subscriberkey, tkn) => {
             "SendTime": "2012-10-05 20:01"
         }
 
-        console.log('Payload SMS: ' + payload);
+        console.log('Payload SMS: ' + JSON.stringify(payload));
     
         const url = process.env.restURL + '/sms/v1/messageContact/MzA6Nzg6MA/send';
 
